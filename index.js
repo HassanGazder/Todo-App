c9227d4c9bbfe5423d6ad1399ca441ce1449380f
HassanGazder <hassan2110c@aptechgdn.net>
2023-09-28 16:09:20 +0500

    update completed;

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ import {
   get,
   set,
   remove,
+  update,
 } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 const dbRef = ref(getDatabase());
-var taskdata = [];
+var taskdata;
 // var userid = 0
 const database = getDatabase();
 const db = getDatabase();
 // var randomnumber = Math.floor(Math.random()*10000)
+var updatedTask;
 let inputtext = document.getElementById("input");
 let addbutton = document.getElementById("addButton");
 let tasklist = document.getElementById("tasklist");
@@ -20,35 +22,7 @@ get(child(dbRef, `alltasks/`))
     if (snapshot.exists()) {
       taskdata = snapshot.val();
       console.log(taskdata);
-      for (let x in taskdata) {
-        console.log(x)
-        let taskdiv = document.createElement("div");
-        taskdiv.className = "task-container";
-
-        let allelements = document.createElement("div");
-
-        let span = document.createElement("span");
-        span.innerText = taskdata[x]["Todolist"];
-
-        let deletetask = document.createElement("button");
-        deletetask.className = "deletetask";
-        deletetask.textContent = "delete";
-        deletetask.addEventListener("click", () => {
-          Deletedata(x);
-        });
-
-        let updatetask = document.createElement("button");
-        updatetask.className = "updatetask";
-        updatetask.textContent = "update";
-
-        allelements.appendChild(span);
-        allelements.appendChild(deletetask);
-        allelements.appendChild(updatetask);
-
-        taskdiv.append(allelements);
-
-        tasklist.appendChild(taskdiv);
-      }
+      renderlist(taskdata);
       console.log(typeof taskdata);
       console.log(taskdata);
     } else {
@@ -58,18 +32,55 @@ get(child(dbRef, `alltasks/`))
   .catch((error) => {
     console.error(error);
   });
+
+function renderlist(taskdata) {
+  console.log("render", taskdata);
+  for (let x in taskdata) {
+    console.log(x);
+    let taskdiv = document.createElement("div");
+    taskdiv.className = "task-container";
+
+    let allelements = document.createElement("div");
+
+    let span = document.createElement("span");
+    span.innerText = taskdata[x]["Todolist"];
+
+    let deletetask = document.createElement("button");
+    deletetask.className = "deletetask";
+    deletetask.textContent = "delete";
+    deletetask.addEventListener("click", () => {
+      Deletedata(x);
+    });
+
+    let updatetask = document.createElement("button");
+    updatetask.className = "updatetask";
+    updatetask.textContent = "update";
+    updatetask.addEventListener("click", () => {
+      Updtdata(x);
+    });
+
+    allelements.appendChild(span);
+    allelements.appendChild(deletetask);
+    allelements.appendChild(updatetask);
+
+    taskdiv.append(allelements);
+
+    tasklist.appendChild(taskdiv);
+  }
+}
 addbutton.addEventListener("click", addUITask);
 
 function addUITask() {
   let tasktext = inputtext.value.trim();
+  let uniquekey;
   if (tasktext === "") {
     console.log("invalid");
     alert("Invalid task");
   } else {
-    console.log(taskdata);
+    // console.log(taskdata);
     let taskdiv = document.createElement("div");
     taskdiv.className = "task-container";
-    let uniquekey = Math.floor(Math.random()*10000)
+    uniquekey = Math.floor(Math.random() * 10000);
 
     let allelements = document.createElement("div");
 
@@ -78,13 +89,16 @@ function addUITask() {
 
     let deletetask = document.createElement("button");
     deletetask.textContent = "delete";
-    deletetask.className = "deletetask"
-    deletetask.addEventListener("click", () => {      
+    deletetask.className = "deletetask";
+    deletetask.addEventListener("click", () => {
       Deletedata(uniquekey);
     });
     let updatetask = document.createElement("button");
     updatetask.className = "updatetask";
     updatetask.textContent = "update";
+    updatetask.addEventListener("click", () => {
+      Updtdata(uniquekey);
+    });
 
     allelements.appendChild(span);
     allelements.appendChild(deletetask);
@@ -94,59 +108,62 @@ function addUITask() {
 
     tasklist.appendChild(taskdiv);
     inputtext.value = "";
+    console.log(uniquekey);
     set(ref(db, "alltasks/" + uniquekey), {
       Todolist: tasktext,
     });
+    console.log("unique", uniquekey);
+    console.log((taskdata[uniquekey] = { ["Todolist"]: tasktext }));
     console.log(taskdata);
-    // console.log(randomnumber)
   }
 }
+function Deletedata(mytask) {
+  console.log(mytask)
+  console.log((delete taskdata[mytask]))
+  console.log(taskdata)
+  tasklist.addEventListener("click", function (e) {
+    if (e.target.className === "deletetask") {
+      e.target.parentElement.remove();
+      console.log("deleted");
+      console.log(mytask);
+    }
+  });
+  console.log("clicked");
+  remove(ref(db, "alltasks/" + mytask))
+  .then(() => {
+    console.log("Data deleted from Firebase");
+  })
+  .catch((error) => {
+    console.error("Error deleting data from Firebase:", error);
+  });
+}
 
-// let element = document.getElementsByTagName("div")[3];
-// element.addEventListener("click", ()=>{
-//  element.remove();
-// })
-tasklist.addEventListener('click',function(e){
-  if(e.target.className === "deletetask" ){
-    e.target.parentElement.remove();
-    console.log("deleted");
+function Updtdata(mytask) {
+  console.log("click");
+  console.log(mytask);
+  
+  updatedTask = prompt("update");
+  console.log((taskdata[mytask] = { ["Todolist"]: updatedTask }));
+
+  if (updatedTask !== null) {
+      
+    update(ref(db, "alltasks/" + mytask), {
+      Todolist: updatedTask,
+    })
+      .then(() => {
+        console.log("Data updated from Firebase");
+      })
+      .catch((error) => {
+        console.error("Error updating data from Firebase:", error);
+      });
+  }
+}
+tasklist.addEventListener("click", function (e) {
+  console.log(taskdata)
+  if (e.target.className === "updatetask") {
+    if (updatedTask !== null) {
+      let spanElement = e.target.parentElement.querySelector("span");
+      spanElement.innerText = updatedTask;
+    }
   }
 })
-function Deletedata(mytask) {
-  console.log(mytask);
-
-  console.log("clicked");
-  remove(ref(db,"alltasks/" + mytask))
-    .then(() => {
-      console.log("Data deleted from Firebase");
-    })
-    .catch((error) => {
-      console.error("Error deleting data from Firebase:", error);
-    });
-}
-
-// tasklist.addEventListener("click", function (e) {
-//   if (e.target.className === "deletetask") {
-//     const taskContainer = e.target.parentElement;
-//     const spanElement = taskContainer.querySelector("span");
-
-//     remove(ref(db,`alltasks/Todolist${taskdata.length}`))
-//       .then(() => {
-//         console.log("Data deleted from Firebase");
-//         taskContainer.remove();
-//       })
-//       .catch((error) => {
-//         console.error("Error deleting data from Firebase:", error);
-//       });
-//   }
-
-//   if (e.target.className === "updatetask") {
-//     let updatedTask = prompt("Update");
-//     if (updatedTask !== null) {
-//       let spanElement = e.target.parentElement.querySelector("span");
-//       spanElement.innerText = updatedTask;
-//     }
-//   }
-// });
-
-// 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
